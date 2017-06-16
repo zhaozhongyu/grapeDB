8cc9c82d2ee254126d89e7fce4535af29d1a6c30
William.Forever <zhaozhonyu@huawei.com>
2017-06-16 17:23:47 +0800

    Create INode.java

---
+++ Index/INode.java
@@ -0,0 +1,14 @@
+package com.zzy.Index;
+
+
+import java.util.ArrayList;
+
+public class INode {
+    public boolean isLeaf = false;
+    public INode parent = null;
+    public ArrayList<Comparable> keys = new ArrayList();
+    public ArrayList<INode> childNodes = new ArrayList();
+
+    public INode() {
+    }
+}
