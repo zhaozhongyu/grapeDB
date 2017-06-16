63d9022fe88290ad22d8ba9fe6cea00cf755cfdc
William.Forever <zhaozhonyu@huawei.com>
2017-06-16 17:23:18 +0800

    Create TreeLeaf.java

---
+++ Index/TreeLeaf.java
@@ -0,0 +1,11 @@
+package com.zzy.Index;
+
+import java.util.ArrayList;
+
+public class TreeLeaf extends INode {
+    public ArrayList<Object> values = new ArrayList();
+    public TreeLeaf rightBrother = null;
+
+    public TreeLeaf() {
+    }
+}
