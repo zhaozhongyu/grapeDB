600faa8125f677fea76980a0ab7d70febc305d3c
William.Forever <zhaozhonyu@huawei.com>
2017-06-20 21:12:11 +0800

    Create ValueString.java

---
+++ src/com/zzy/Value/ValueString.java
@@ -0,0 +1,33 @@
+package com.zzy.Value;
+
+public class ValueString extends Value
+{
+    private String value;
+
+    public ValueString(String str){
+        this.value = str;
+    }
+    @Override
+    public int getType()
+    {
+        return this.STRING;
+    }
+
+    @Override
+    public String getValue(){
+        return this.value;
+    }
+
+    @Override public int compareTo(Object o)
+    {
+        return this.getValue().compareTo(((ValueString)o).getValue());
+    }
+
+    public String toString(){
+        return this.getValue();
+    }
+
+    public boolean equals(Object vs){
+        return this.getValue().equals(((ValueString)vs).getValue());
+    }
+}
