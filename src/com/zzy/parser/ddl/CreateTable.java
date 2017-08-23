f4e91eb8a115ccc5e613c129d1d06262a782146f
William.Forever <zhaozhonyu@huawei.com>
2017-08-23 19:58:06 +0800

    Update CreateTable.java

--- src/com/zzy/parser/ddl/CreateTable.java
+++ src/com/zzy/parser/ddl/CreateTable.java
@@ -3,23 +3,34 @@ package com.zzy.parser.ddl;
 import com.zzy.Schema.Schema;
 import com.zzy.Table.Column;
 import com.zzy.Table.Table;
+import com.zzy.engine.SQLConnection;
+import com.zzy.engine.SQLResultSet;
+import com.zzy.engine.SQLStatement;
+import com.zzy.parser.Prepared;
 
+import java.sql.ResultSet;
 import java.util.LinkedList;
 
 /**
- *
  * 通过这个类来创建table
  */
-public class CreateTable
+public class CreateTable extends Prepared
 {
     private String tableName;
     public LinkedList<Column> columns ;
-    public Schema schema;
     public Column primarykey;
+    private Schema schema;
 
-    public CreateTable(Schema schema){
-        this.schema =schema;
+    public CreateTable(SQLConnection connection, Schema schema){
+        super(connection);
         columns = new LinkedList<>();
+        this.schema = schema;
+    }
+
+    @Override
+    public SQLResultSet execute(SQLStatement statement) {
+        create();
+        return null;
     }
 
     public void addColumn(Column column){
@@ -33,8 +44,11 @@ public class CreateTable
             return table;
         }
         Table table = new Table(tableName, schema, (Column[])columns.toArray() );
+
         return table;
     }
+
+
 
     public void setTableName(String tableName)
     {
