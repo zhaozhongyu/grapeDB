2eeb8efedfbfd0369589a473c40f4dd3219689ca
William.Forever <zhaozhonyu@huawei.com>
2017-08-25 19:38:02 +0800

    Update Row.java

--- src/com/zzy/Table/Row.java
+++ src/com/zzy/Table/Row.java
@@ -2,6 +2,8 @@ package com.zzy.Table;
 
 import com.zzy.Value.Value;
 
+import java.util.Arrays;
+
 public class Row
 {
     private Value[] data;      //标识实际的一组数据, 以数组方式存放
@@ -14,9 +16,12 @@ public class Row
         return data[i];
     }
 
+
     public void setValue(int i, Value v) {
         data[i] = v;
     } //update的时候使用
+
+    public int length(){ return data.length; }
 
     public String toString(){
         StringBuilder sb = new StringBuilder();
@@ -28,4 +33,27 @@ public class Row
         return sb.toString();
     }
 
+
+    @Override
+    public boolean equals(Object o){
+        Row row = (Row)o;
+        if(row.length() != length()){
+            return false;
+        }
+        for(int i = 0; i < length(); i ++){
+            if(! getValue(i).equals(row.getValue(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode(){
+        int code = 0;
+        for(int i = 0; i < length(); i++){
+            code += getValue(i).hashCode() * (i+1);
+        }
+        return code;
+    }
 }
